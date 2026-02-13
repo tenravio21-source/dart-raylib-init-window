01c79875f1a911f7b997fac6732a8d9b63ce2d90
hitachi-payments <hitachi-payments@gmail.com>
2026-02-13 17:30:59 +0530

    basic window

--- bin/game.dart
+++ bin/game.dart
@@ -1,5 +1,4 @@
 import 'package:game/configs/configs.dart' show Configs;
-import 'package:game/player/player.dart' show Player;
 import 'package:raylib/raylib.dart';
 
 void main() {
@@ -7,22 +6,11 @@ void main() {
 
   initWindow(Configs.screenWidth, Configs.screenHeight, Configs.title);
 
-  final Player player = Player(
-    position: Vector2(Configs.screenWidth / 2, Configs.screenHeight / 2),
-  );
-
   setTargetFPS(60);
 
   while (!windowShouldClose()) {
-    player.update();
-
-    // 2. DRAW: Render the frame
     beginDrawing();
     clearBackground(Color.rayWhite);
-
-    drawText('Use WASD to move the player!', 10, 10, 20, Color.darkGray);
-
-    player.draw();
 
     endDrawing();
   }
